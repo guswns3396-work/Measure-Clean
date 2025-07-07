d76e59c8a19e34790b14a81e4bac1b3ec50b03c4
Hyun-Joon Yang <yanghyun@stanford.edu>
2025-07-07 14:35:07 -0700

    implemented gad7

--- measure-clean/clinical/gad7.py
+++ measure-clean/clinical/gad7.py
@@ -3,4 +3,24 @@ from ..measure import Measure
 import numpy as np
 import pandas as pd
 
-#
+
+# GAD-7 (General Anxiety Disorder-7)
+
+class GAD7(Measure):
+    @classmethod
+    def get_prefix(cls):
+        return 'gad7'
+
+    @classmethod
+    def get_cols(cls):
+        return [f"{cls.get_prefix()}_{i + 1}" for i in range(7)]
+
+    @classmethod
+    def check_range(cls, df):
+        vals = [i in range(0, 3 + 1)]
+        return np.argwhere(~df.isin(vals + [np.nan]))
+
+    @classmethod
+    def score(cls, df):
+        score = df.sum(axis=1, skipna=False)
+        return score
