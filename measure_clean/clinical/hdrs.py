8b69d7c3e723ec9a5d53bb5026940a54336ca990
Hyun-Joon Yang <yanghyun@stanford.edu>
2025-07-08 12:27:27 -0700

    implemented hdrs

---
+++ measure_clean/clinical/hdrs.py
@@ -0,0 +1,45 @@
+from ..measure import Measure
+
+import numpy as np
+import pandas as pd
+
+
+# Hamilton Rating Scale for Depression, also known as the Hamilton Depression Rating Scale, or HAM-D
+# https://www.med.upenn.edu/cbti/assets/user-content/documents/Hamilton%20Rating%20Scale%20for%20Depression%20(HAM-D).pdf
+
+class HDRS(Measure):
+    @classmethod
+    def score(cls, df):
+        df = df[[f"{cls.get_prefix()}_{i}" for i in range(1, 17 + 1)]]
+        return df.sum(axis=1, skipna=False)
+
+    @classmethod
+    def check_range(cls, df):
+        lik04 = [1, 2, 3, 7, 8, 9, 10, 11, 15, 19, 20]
+        lik02 = [4, 5, 6, 12, 13, 14, 16, 17, 18, 21]
+        idx = []
+        for cols, vals in zip([lik04, lik02], [range(0, 4 + 1), range(0, 2 + 1)]):
+            cols = cls.subset_cols_num(df.columns, cols, fr"{cls.get_prefix()}_(\d+)")
+            idx.append(cls.argwhere(~df[cols].isin([i for i in vals] + [np.nan])))
+        idx = np.concatenate(idx, axis=0)
+        return idx
+
+
+class HDRS21(HDRS):
+    @classmethod
+    def get_prefix(cls):
+        return 'hdrs21'
+
+    @classmethod
+    def get_cols(cls):
+        return [f"{cls.get_prefix()}_{i + 1}" for i in range(21)]
+
+
+class HDRS17(HDRS):
+    @classmethod
+    def get_prefix(cls):
+        return 'hdrs17'
+
+    @classmethod
+    def get_cols(cls):
+        return [f"{cls.get_prefix()}_{i + 1}" for i in range(17)]
---
+++ tests/test_hdrs.py
@@ -0,0 +1,64 @@
+import unittest
+import numpy as np
+import pandas as pd
+from measure_clean.measure import Measure
+from measure_clean.clinical.hdrs import HDRS21, HDRS17
+
+
+# TODO: test other versions of HDRS than HDRS21
+class TestCheckRange(unittest.TestCase):
+
+    def setUp(self):
+        self.TestMeasure = HDRS21()
+
+    def test__standard__check_range(self):
+        np.random.seed(0)
+        lik04 = [1, 2, 3, 7, 8, 9, 10, 11, 15, 19, 20]
+        lik02 = [4, 5, 6, 12, 13, 14, 16, 17, 18, 21]
+        df1 = pd.DataFrame(
+            np.random.randint(low=0, high=6, size=(5, len(lik04))),
+            columns=[f"{self.TestMeasure.get_prefix()}_{i}" for i in lik04]
+        )
+        df2 = pd.DataFrame(
+            np.random.randint(low=0, high=4, size=(5, len(lik02))),
+            columns=[f"{self.TestMeasure.get_prefix()}_{i}" for i in lik02]
+        )
+        df = pd.concat([df1, df2], axis=1)
+        tmp = df[[f"{self.TestMeasure.get_prefix()}_{i}" for i in range(1, 21 + 1)]]
+        target = [[0, 1], [3, 1], [2, 3], [1, 4], [0, 5], [4, 5], [2, 8], [1, 10],
+                  [2, 11], [3, 11], [1, 12], [3, 12], [4, 12], [0, 14], [4, 14], [1, 15], [3, 15], [4, 15],
+                  [0, 16], [1, 16], [4, 16], [1, 18], [4, 18], [2, 20]]
+        source = self.TestMeasure.check_range(df)
+        target = set([
+            tuple([tmp.index[x[0]], tmp.columns[x[1]]])
+            for x in target
+        ])
+        source = set([tuple(x) for x in source.tolist()])
+        self.assertEqual(target, source)
+
+
+class TestScore(unittest.TestCase):
+
+    def setUp(self):
+        self.TestMeasure = HDRS21()
+
+    def test__standard__score(self):
+        np.random.seed(0)
+        lik04 = [1, 2, 3, 7, 8, 9, 10, 11, 15, 19, 20]
+        lik02 = [4, 5, 6, 12, 13, 14, 16, 17, 18, 21]
+        df1 = pd.DataFrame(
+            np.random.randint(low=0, high=5, size=(5, len(lik04))),
+            columns=[f"{self.TestMeasure.get_prefix()}_{i}" for i in lik04]
+        )
+        df2 = pd.DataFrame(
+            np.random.randint(low=0, high=3, size=(5, len(lik02))),
+            columns=[f"{self.TestMeasure.get_prefix()}_{i}" for i in lik02]
+        )
+        df = pd.concat([df1, df2], axis=1)
+        target = [34, 19, 25, 27, 26]
+        source = self.TestMeasure.score(df)
+        self.assertTrue((target == source).all())
+
+
+if __name__ == "__main__":
+    unittest.main()
